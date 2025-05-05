53b5339b40708dfd0e31dbeb20c98c7bf3d1040e
GabrielReuziault <gabriel.reuziault@caensup.fr>
2025-05-05 22:08:31 +0200

    Update ServiceHero.tsx

--- src/components/ServiceHero.tsx
+++ src/components/ServiceHero.tsx
@@ -4,8 +4,7 @@ import { motion } from "framer-motion";
 export default function ServiceHero() {
   return (
     <motion.div
-      className="text-center py-20 bg-gradient-to-r from-neutral-100 to-neutral-50
-                  rounded-lg"
+      className="text-center py-20 bg-gradient-to-br from-secondary to-primary text-white rounded-lg"
       initial={{ opacity: 0, y: -20 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
